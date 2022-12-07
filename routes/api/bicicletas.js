5949aa3ea4d9aabffbf553875d4560b00f195bc3
e1230 <embeltrans@correo.udistrital.edu.co>
2022-12-07 08:46:37 -0500

    put

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -5,4 +5,5 @@ let bicicletaController = require("../../controllers/api/bicicletaApiController"
 router.get("/", bicicletaController.bicicleta_list);
 router.post("/create", bicicletaController.bicicleta_create);
 router.delete("/delete", bicicletaController.bicicleta_delete);
+router.put("/:id/update", bicicletaController.bicicleta_update);
 module.exports = router;
